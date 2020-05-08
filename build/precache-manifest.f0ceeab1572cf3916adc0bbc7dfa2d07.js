ebbde4b9eacf49b659ac89cde15e7e1cb0278216
Akshay Raichur <lordakshay99@gmail.com>
2020-05-09 02:37:49 +0530

    Final App

---
+++ build/precache-manifest.f0ceeab1572cf3916adc0bbc7dfa2d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895a23e44b9262bfb7d74656739a9f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c782cfc6f37be45731f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37069ff8df16580d7419",
+    "url": "/static/js/2.0a60cb0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0a60cb0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c782cfc6f37be45731f",
+    "url": "/static/js/main.11b3f59b.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  }
+]);
